cbb4a685dc8f58dd70d2eb7bd656dc4740c2ffd6
Shingugitvr000 <Shingugitvr000@gmail.com>
2026-04-15 12:02:08 +0900

    [Feat] 카드 매니저 추가, 마나 사용까지 완료

--- Assets/Scripts/CardGame/CharacterStats.cs
+++ Assets/Scripts/CardGame/CharacterStats.cs
@@ -13,10 +13,18 @@ public class CharacterStats : MonoBehaviour
     public Slider healthBar;
     public TextMeshProUGUI healthText;
 
+    //새로 추가되는 마나 변수 
+    public int maxMana = 10;                                    //최대 마나
+    public int currentMana;                                     //현재 마나
+    public Slider manaBar;                                      //마나 바 UI
+    public TextMeshProUGUI manaText;                            //마나 텍스트 UI
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentHealth = maxHealth;
+        currentMana = maxMana;
+        UpdateUI();
     }
 
     public void TakeDamage(int damage)
@@ -27,5 +35,51 @@ public class CharacterStats : MonoBehaviour
     public void Heal(int amount)
     {
         currentHealth += amount;
+    }
+
+    public void UseMana(int amount)
+    {
+        currentMana -= amount;
+        if (currentMana < 0)
+        {
+            currentMana = 0;
+        }
+        UpdateUI();
+    }
+
+    public void GainMana(int amount)
+    {
+        currentMana += amount;
+        
+        if (currentMana > maxMana)
+        {
+            currentMana = maxMana;
+        }
+        UpdateUI();
+    }
+
+
+
+    private void UpdateUI()
+    {
+        if (healthBar != null)
+        {
+            healthBar.value = (float)currentHealth / maxHealth;
+        }
+
+        if (healthText != null)
+        {
+            healthText.text = $"{currentHealth} / {maxHealth}";
+        }
+
+        if (manaBar != null)
+        {
+            manaBar.value = (float)currentMana / maxMana;
+        }
+
+        if (manaText != null)
+        {
+            manaText.text = $"{currentMana} / {maxMana}";
+        }
     }
 }
