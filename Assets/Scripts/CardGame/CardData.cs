81313a9b3dec513dd27c87b1ffbed10bacc66baa
Shingugitvr000 <Shingugitvr000@gmail.com>
2026-04-29 12:07:56 +0900

    [Feat] 카드 추가 효과 구현

--- Assets/Scripts/CardGame/CardData.cs
+++ Assets/Scripts/CardGame/CardData.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "NewCard" , menuName = "Card/Card Data")]
 public class CardData : ScriptableObject
@@ -17,6 +18,19 @@ public class CardData : ScriptableObject
     public int manaCost;                                    //마나 비용
     public int effectAmount;                                //효과 값 (공격력)
     public CardType cardType;                               //카드 타입
+
+    //추가 효과 리스트
+    public List<AdditionalEffect> additionalEffects = new List<AdditionalEffect>();
+
+    public enum AdditionalEffectType                        //추가 효과 타입 열거형 추가 
+    {   
+        None,                                               //추가 효과 없음
+        DrawCard,                                           //카드 드로우
+        DiscardCard,                                        //카드 버리기
+        GainMana,                                           //마나 획득
+        ReduceEnemyMana,                                    //적 마나 감소
+        ReduceCardCost                                      //다음 카드 비용 감소 
+    }
 
     public Color GetCardColor()                             //타입에 따른 카드 색상
     {
@@ -37,5 +51,21 @@ public class CardData : ScriptableObject
             default:
                 return Color.white;
         }
+    }
+
+    //추가 효과 정보를 문자열로 변환 
+    public string GetAdditionalEffectDescription()
+    {
+        if (additionalEffects.Count == 0)
+            return "";
+
+        string result = "\n";
+
+        foreach(var effect in additionalEffects)
+        {
+            result += effect.GetDescription() + "\n";
+        }
+
+        return result;
     }
 }
