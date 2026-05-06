722309c60733ddf2f50b64d6774d8fdd01efa0b3
Shingugitvr000 <Shingugitvr000@gmail.com>
2026-05-06 12:00:03 +0900

    [Feat] 데미지 텍스트 표시 시스템 제작

--- Assets/Scripts/CardGame/CharacterStats.cs
+++ Assets/Scripts/CardGame/CharacterStats.cs
@@ -30,11 +30,25 @@ public class CharacterStats : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        if(DamageEffectManager.instance != null)
+        {
+            Vector3 position = transform.position;
+            position += new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(1f, 1.5f), 0);
+
+            DamageEffectManager.instance.ShowDamage(position, damage, false);
+        }
     }
 
     public void Heal(int amount)
     {
         currentHealth += amount;
+        if (DamageEffectManager.instance != null)
+        {
+            Vector3 position = transform.position;
+            position += new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(1f, 1.5f), 0);
+
+            DamageEffectManager.instance.ShowHeal(position, amount, false);
+        }
     }
 
     public void UseMana(int amount)
